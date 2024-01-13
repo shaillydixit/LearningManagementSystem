be9dd43f5b2693cb1bafd735ed533e91b86185d7
Work <ShaillyWork@Shaillys-Mac-mini.local>
2024-01-14 03:27:21 +0530

    admn dashboard with profile edit and update password created

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,14 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
     Route::middleware(['auth','roles:admin'])->group(function(){
         Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+        Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+        Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+        Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+        Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+        Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
     });
+    Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
     Route::middleware(['auth','roles:instructor'])->group(function(){
     Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
     });
