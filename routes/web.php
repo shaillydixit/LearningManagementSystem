5ad02a84fe461ce4988caae95d8ed844334a181b
Work <ShaillyWork@Shaillys-Mac-mini.local>
2024-01-23 05:00:14 +0530

    instructor profile added

--- routes/web.php
+++ routes/web.php
@@ -80,3 +80,32 @@ Route::controller(CategoryController::class)->group(function(){
     Route::get('/delete/subcategory/{id}','DeleteSubCategory')->name('delete.subcategory');
 
 });
+// Instructor All Route 
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/all/instructor','AllInstructor')->name('all.instructor');
+    Route::post('/update/user/stauts','UpdateUserStatus')->name('update.user.stauts');
+
+});
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+Route::get('/become/instructor', [AdminController::class, 'BecomeInstructor'])->name('become.instructor');
+Route::post('/instructor/register', [AdminController::class, 'InstructorRegister'])->name('instructor.register');
+
+
+///// Instructor Group Middleware
+Route::middleware(['auth','roles:instructor'])->group(function(){
+  
+Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+Route::get('/instructor/logout', [InstructorController::class, 'InstructorLogout'])->name('instructor.logout');
+
+Route::get('/instructor/profile', [InstructorController::class, 'InstructorProfile'])->name('instructor.profile');
+Route::post('/instructor/profile/store', [InstructorController::class, 'InstructorProfileStore'])->name('instructor.profile.store');
+
+Route::get('/instructor/change/password', [InstructorController::class, 'InstructorChangePassword'])->name('instructor.change.password');
+Route::post('/instructor/password/update', [InstructorController::class, 'InstructorPasswordUpdate'])->name('instructor.password.update');
+
+}); // End Instructor Group Middleware 
+
+Route::get('/instructor/login', [InstructorController::class, 'InstructorLogin'])->name('instructor.login');
